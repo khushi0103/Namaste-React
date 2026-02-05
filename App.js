cbcef189b297cdd5bb5d15f622fd765854a35911
Khushi Yadav <yadavkhushi0103@gmail.com>
2026-02-05 17:47:46 +0530

    Studying about jsx , components.

--- App.js
+++ App.js
@@ -29,6 +29,8 @@ const parent = React.createElement("div" ,
 
 // creating H1 tag using react and heading variable contain a object (javascript object) , react h1 element
 // {} -> contain attributes
+// create h1 element using core react
+// from now we will never use React.createElement in future
 const heading = React.createElement("h1", 
         {id: "heading"}, "Hello World from React!");
 
@@ -40,4 +42,59 @@ root.render(heading);
 
 
                   //  <<<<<----------------- LECTURE 2 --------------------->>>>
+
+
+// JSX -->  create h1 tag using jsx . It is not HTML inside JS it is html like syntx or XML like syntax.
+// this is not proper js browser not able to understand this but how then it work bcz of parcel
+ //this JSX transpiled before it reaches the JS Engine, it is done by Babel ( package) cones under parcel
+
+ //React Element
+const jsxHeading = (
+    <h1 id = "heading">                 
+    Namaste React using JSX</h1> 
+); 
+
+    // React Component------>there are two types of component - Class Based Component - OLD , Functional Component - NEW (today use this)
+
+   
+
+
+
+    // Functional Component give name as a capital otherwise it give error 
+    // A FUNCTION WHICH RETURN REACT ELEMENT(JSX element) IS CALLED FUNCTIONAL COMPONENT
+
+    // to add another component in one component 
+    // here we use arrrow function
+
+    const title = (
+        <h1 className="head">
+            Namaste
+        </h1>
+    )
+
+    const Title = () => (
+        <h1 className = "head" tabIndex= "5">
+            Namaste react
+        </h1>
+    );
+
+    
+      
+    const HeadingComponent = () => (
+        <div id = "container">
+          // to put another component inside this component
+          // Title at the end ek function h js ka aur hm any javascript code ko {} curly bracket mai likh skte h 
+          {Title()}
+          <Title />  
+          <Title></Title>
+          // to put react element inside component bcz at the end title is javascript variable that contain object 
+          {title}  
+          <h1> Namaste React Functional Component</h1> 
+      </div>
+    )
+
+    const root1 = ReactDOM.createRoot(document.getElementById("root"));
+
+    // To render functional component use
+    root.render(<HeadingComponent />)
 
